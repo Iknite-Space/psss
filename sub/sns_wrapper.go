0c7b1b22ea09a9ffc194286db43fe2c659c3adae
Ryan J. Yoder <ryan@iknite.studio>
2025-09-30 11:49:01 +0100

    sns func

--- sub/sns_wrapper.go
+++ sub/sns_wrapper.go
@@ -1,7 +1,13 @@
 package sub
 
+import "context"
+
 type SnsWrapper struct {
 	Message string `json:"Message"`
 }
 
-func handleToJSON(c)
+type StringHandler func(context.Context, string) error
+
+func handleToJSON(handler StringHandler) func(context.Context, SnsWrapper) error {
+	return nil
+}
