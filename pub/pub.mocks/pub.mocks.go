9899e329a86ae865867d4b273eaf8d63cd4de7fa
Namkat Cedrick <namkatcedrickjumtock3@gmail.com>
2025-10-04 13:19:22 +0100

    update nit impl

--- pub/pub.mocks/pub.mocks.go
+++ pub/pub.mocks/pub.mocks.go
@@ -8,21 +8,13 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// MockPublisherImplementation is a manual mock implementation of Publisher[T]
-type MockPublisherImplementation[T proto.Message] struct {
-	PublishMockFunc func(ctx context.Context, message models.ProtoMutationEvent[T]) error
+// MockPublisherImpl is a manual mock implementation of Publisher[T]
+type MockPublisherImpl[T proto.Message] struct {
 }
 
-var _ pub.Publisher[proto.Message] = (*MockPublisherImplementation[proto.Message])(nil)
-
-// NewMockPublisher creates a new instance of MockPublisherImplementation[T]
-func NewMockPublish[T proto.Message](Publish func(ctx context.Context, message models.ProtoMutationEvent[T]) error) *MockPublisherImplementation[T] {
-	return &MockPublisherImplementation[T]{
-		PublishMockFunc: Publish,
-	}
-}
+var _ pub.Publisher[proto.Message] = (*MockPublisherImpl[proto.Message])(nil)
 
 // MockPublish sets the mock implementation for the Publish method
-func (m *MockPublisherImplementation[T]) Publish(ctx context.Context, message models.ProtoMutationEvent[T]) error {
-	return m.Publish(ctx, message)
+func (m *MockPublisherImpl[T]) Publish(ctx context.Context, message models.ProtoMutationEvent[T]) error {
+	return nil
 }
