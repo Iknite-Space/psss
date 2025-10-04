88891acec23edd672b3e94771505fd83fd370671
Namkat Cedrick <namkatcedrickjumtock3@gmail.com>
2025-10-04 13:21:29 +0100

    update nit impl

--- pub/pub.mocks/pub.mocks.go
+++ pub/pub.mocks/pub.mocks.go
@@ -8,13 +8,13 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// MockPublisherImpl is a manual mock implementation of Publisher[T]
-type MockPublisherImpl[T proto.Message] struct {
+// MockPublisher is a manual mock implementation of Publisher[T]
+type MockPublisher[T proto.Message] struct {
 }
 
-var _ pub.Publisher[proto.Message] = (*MockPublisherImpl[proto.Message])(nil)
+var _ pub.Publisher[proto.Message] = (*MockPublisher[proto.Message])(nil)
 
 // MockPublish sets the mock implementation for the Publish method
-func (m *MockPublisherImpl[T]) Publish(ctx context.Context, message models.ProtoMutationEvent[T]) error {
+func (m *MockPublisher[T]) Publish(ctx context.Context, message models.ProtoMutationEvent[T]) error {
 	return nil
 }
